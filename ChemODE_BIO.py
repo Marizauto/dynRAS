fccbc777a985936223b5a175ff1fece2715660c4
Marizauto <128140640+Marizauto@users.noreply.github.com>
2024-06-17 14:25:03 +0200

    update

--- ChemODE_BIO.py
+++ ChemODE_BIO.py
@@ -8,7 +8,7 @@ def chemODE_BIO(Tank,params,t):
     d_NOB=Growth[3]
     day = t / 60 / 60 / 24
     #K1=(johnson 1982)
-# simulation Jafari et al. 2024
+    # simulation of the experimental set up from Jafari et al. 2024
     if day <= 14:
         T_HCO3 = 200/50.04
         feedback_term_HCO3 = (1 + (Tank.HCO3 / T_HCO3) ** 10)
@@ -153,40 +153,8 @@ def chemODE_BIO_NaOH(Tank, params, t):
     dNOB=g_NOB-d_NOB
     dY = np.array([dCO2aq, dHCO3, dCO32, dH, dOH, dNH4, dNH3,dNO2,dAOB,dNOB])
     return dY
-
-def chemODE_BIO_pH_control(Tank, params, t):
-    #set the pH with either NaOH or HCO3
-    Growth=growth_bact(Tank,params)
-    g_AOB=Growth[0]
-    g_NOB=Growth[1]
-    d_AOB=Growth[2]
-    d_NOB=Growth[3]
-   #add_OH = 0 #comment or uncomment this line to turn on or off pH control using NaOH
-    desired_pH = 7
-    T_H = (10 ** -desired_pH) * 10 ** 3 #dosing with OH
-    add_OH = 0  # comment or uncomment this line to turn on or off pH control using NaOH
-    feedback_term_OH = (1 + (T_H / Tank.H) ** 10) #dosing with OH
-    #add_OH = (0.0001* Tank.dosing_OH.OH / Tank.V) / feedback_term_OH #dosing with OH
-    Tank.dosing_time.append(t)
-    Tank.dosing_amount_OH.append(add_OH)
-    T_HCO3 = (10 ** (desired_pH - (-np.log10(0.79*10**-6)))) * Tank.CO2aq  # dosing with HCO3
-    feedback_term_HCO3 = (1 + (Tank.HCO3 / T_HCO3) ** 10)  # dosing with HCO3
-    add_HCO3 = (0.0001 * Tank.dosing_HCO3.HCO3 / Tank.V) / feedback_term_HCO3  # dosing with HCO3
-    #add_HCO3 = 0 #comment or uncomment this line to turn on or off pH control using HCO3
-    Tank.dosing_amount_HCO3.append(add_HCO3)
-    dCO2aq = (params.k_1*Tank.H)*Tank.HCO3-(params.k1)*Tank.CO2aq+(Tank.Tminus.CO2aq-Tank.CO2aq)*params.F/Tank.V
-    dHCO3 = params.k1*Tank.CO2aq-(params.k_1*Tank.H)*Tank.HCO3+params.k_H*Tank.H*Tank.CO32-params.kH*Tank.HCO3-params.muAOB*(Tank.NH3/(params.KNH3+Tank.NH3))*(Tank.HCO3/(params.KAlk+Tank.HCO3))*Tank.AOB*1/(params.YAOB/((1/14)/(7/61.1)))/Tank.V+add_HCO3+(Tank.Tminus.HCO3-Tank.HCO3)*params.F/Tank.V
-    dCO32 =params.kH*Tank.HCO3-params.k_H*Tank.H*Tank.CO32+(Tank.Tminus.CO32-Tank.CO32)*params.F/Tank.V
-    dH = params.k4*Tank.NH4-params.k_4*Tank.NH3*Tank.H+params.k1*Tank.CO2aq-params.k_1*Tank.H*Tank.HCO3+params.kH*Tank.HCO3-params.k_H*Tank.H*Tank.CO32+params.k3-params.k_3*Tank.H*Tank.OH+(Tank.Tminus.H-Tank.H)*params.F/Tank.V
-    dOH = params.k3-params.k_3*Tank.H*Tank.OH+add_OH+(Tank.Tminus.OH-Tank.OH)*params.F/Tank.V
-    dNH4 =(-params.k4*Tank.NH4)+params.k_4*Tank.NH3*Tank.H+(Tank.Tminus.NH4-Tank.NH4)*params.F/Tank.V
-    dNH3 = params.k4*Tank.NH4-params.k_4*Tank.NH3*Tank.H-params.muAOB*(Tank.NH3/(params.KNH3+Tank.NH3))*(Tank.HCO3/(params.KAlk+Tank.HCO3))*Tank.AOB/Tank.V*(1/params.YAOB)+(Tank.Tminus.NH3-Tank.NH3)*params.F/Tank.V
-    dNO2=+params.muAOB*(Tank.NH3/(params.KNH3+Tank.NH3))*(Tank.HCO3/(params.KAlk+Tank.HCO3))*Tank.AOB/Tank.V*1/params.YAOB-params.muNOB*(Tank.NO2/(Tank.NO2+params.KNO2))*Tank.NOB/Tank.V*1/params.YNOB+(Tank.Tminus.NO2-Tank.NO2)*params.F/Tank.V
-    dAOB=g_AOB-d_AOB
-    dNOB=g_NOB-d_NOB
-    dY = np.array([dCO2aq, dHCO3, dCO32, dH, dOH, dNH4, dNH3,dNO2,dAOB,dNOB])
-    return dY
 def chemODE_BIO_alk_control(Tank, params, t):
+    #simulation case 3
     #set alkalnity with either NaOH or HCO3 based on the CO2 in the system
     Growth=growth_bact(Tank,params)
     g_AOB=Growth[0]
@@ -207,6 +175,39 @@ def chemODE_BIO_alk_control(Tank, params, t):
     Tank.dosing_time.append(t)
     Tank.dosing_amount_OH.append(add_OH)
     Tank.dosing_amount_HCO3.append(add_HCO3)
+    dHCO3 = params.k1*Tank.CO2aq-(params.k_1*Tank.H)*Tank.HCO3+params.k_H*Tank.H*Tank.CO32-params.kH*Tank.HCO3-params.muAOB*(Tank.NH3/(params.KNH3+Tank.NH3))*(Tank.HCO3/(params.KAlk+Tank.HCO3))*Tank.AOB*1/(params.YAOB/((1/14)/(7/61.1)))/Tank.V+add_HCO3+(Tank.Tminus.HCO3-Tank.HCO3)*params.F/Tank.V
+    dCO32 =params.kH*Tank.HCO3-params.k_H*Tank.H*Tank.CO32+(Tank.Tminus.CO32-Tank.CO32)*params.F/Tank.V
+    dH = params.k4*Tank.NH4-params.k_4*Tank.NH3*Tank.H+params.k1*Tank.CO2aq-params.k_1*Tank.H*Tank.HCO3+params.kH*Tank.HCO3-params.k_H*Tank.H*Tank.CO32+params.k3-params.k_3*Tank.H*Tank.OH+(Tank.Tminus.H-Tank.H)*params.F/Tank.V
+    dOH = params.k3-params.k_3*Tank.H*Tank.OH+add_OH+(Tank.Tminus.OH-Tank.OH)*params.F/Tank.V
+    dNH4 =(-params.k4*Tank.NH4)+params.k_4*Tank.NH3*Tank.H+(Tank.Tminus.NH4-Tank.NH4)*params.F/Tank.V
+    dNH3 = params.k4*Tank.NH4-params.k_4*Tank.NH3*Tank.H-params.muAOB*(Tank.NH3/(params.KNH3+Tank.NH3))*(Tank.HCO3/(params.KAlk+Tank.HCO3))*Tank.AOB/Tank.V*(1/params.YAOB)+(Tank.Tminus.NH3-Tank.NH3)*params.F/Tank.V
+    dNO2=+params.muAOB*(Tank.NH3/(params.KNH3+Tank.NH3))*(Tank.HCO3/(params.KAlk+Tank.HCO3))*Tank.AOB/Tank.V*1/params.YAOB-params.muNOB*(Tank.NO2/(Tank.NO2+params.KNO2))*Tank.NOB/Tank.V*1/params.YNOB+(Tank.Tminus.NO2-Tank.NO2)*params.F/Tank.V
+    dAOB=g_AOB-d_AOB
+    dNOB=g_NOB-d_NOB
+    dY = np.array([dCO2aq, dHCO3, dCO32, dH, dOH, dNH4, dNH3,dNO2,dAOB,dNOB])
+    return dY
+
+def chemODE_BIO_pH_control(Tank, params, t):
+    #set the pH with either NaOH or HCO3 (supplementary figure)
+    Growth=growth_bact(Tank,params)
+    g_AOB=Growth[0]
+    g_NOB=Growth[1]
+    d_AOB=Growth[2]
+    d_NOB=Growth[3]
+   #add_OH = 0 #comment or uncomment this line to turn on or off pH control using NaOH
+    desired_pH = 7
+    T_H = (10 ** -desired_pH) * 10 ** 3 #dosing with OH
+    add_OH = 0  # comment or uncomment this line to turn on or off pH control using NaOH
+    feedback_term_OH = (1 + (T_H / Tank.H) ** 10) #dosing with OH
+    #add_OH = (0.0001* Tank.dosing_OH.OH / Tank.V) / feedback_term_OH #dosing with OH
+    Tank.dosing_time.append(t)
+    Tank.dosing_amount_OH.append(add_OH)
+    T_HCO3 = (10 ** (desired_pH - (-np.log10(0.79*10**-6)))) * Tank.CO2aq  # dosing with HCO3
+    feedback_term_HCO3 = (1 + (Tank.HCO3 / T_HCO3) ** 10)  # dosing with HCO3
+    add_HCO3 = (0.0001 * Tank.dosing_HCO3.HCO3 / Tank.V) / feedback_term_HCO3  # dosing with HCO3
+    #add_HCO3 = 0 #comment or uncomment this line to turn on or off pH control using HCO3
+    Tank.dosing_amount_HCO3.append(add_HCO3)
+    dCO2aq = (params.k_1*Tank.H)*Tank.HCO3-(params.k1)*Tank.CO2aq+(Tank.Tminus.CO2aq-Tank.CO2aq)*params.F/Tank.V
     dHCO3 = params.k1*Tank.CO2aq-(params.k_1*Tank.H)*Tank.HCO3+params.k_H*Tank.H*Tank.CO32-params.kH*Tank.HCO3-params.muAOB*(Tank.NH3/(params.KNH3+Tank.NH3))*(Tank.HCO3/(params.KAlk+Tank.HCO3))*Tank.AOB*1/(params.YAOB/((1/14)/(7/61.1)))/Tank.V+add_HCO3+(Tank.Tminus.HCO3-Tank.HCO3)*params.F/Tank.V
     dCO32 =params.kH*Tank.HCO3-params.k_H*Tank.H*Tank.CO32+(Tank.Tminus.CO32-Tank.CO32)*params.F/Tank.V
     dH = params.k4*Tank.NH4-params.k_4*Tank.NH3*Tank.H+params.k1*Tank.CO2aq-params.k_1*Tank.H*Tank.HCO3+params.kH*Tank.HCO3-params.k_H*Tank.H*Tank.CO32+params.k3-params.k_3*Tank.H*Tank.OH+(Tank.Tminus.H-Tank.H)*params.F/Tank.V
--- Class_definition_and_solver.py
+++ Class_definition_and_solver.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from ChemODE_BIO import chemODE_BIO #simulation case 1 modify uncomment function for the right hand side of for the ODE of the biofilter class to run it
-from ChemODE_BIO import chemODE_BIO_HCO3 #simulation case 2 HCO3 dosing only uncomment the function for the right hand side of for the ODE of the biofilter class to run it
-from ChemODE_BIO import chemODE_BIO_NaOH #simulation case 2 NaOH dosing only uncomment the function for the right hand side of for the ODE of the biofilter class to run it
+from ChemODE_BIO import chemODE_BIO_HCO3 #simulation case 1 HCO3 dosing only uncomment the function for the right hand side of for the ODE of the biofilter class to run it
+from ChemODE_BIO import chemODE_BIO_NaOH #simulation case 1 NaOH dosing only uncomment the function for the right hand side of for the ODE of the biofilter class to run it
+from ChemODE_BIO import chemODE_BIO_alk_control
 from ChemODE_BIO import chemODE_BIO_pH_control #set a pH threshold and dose NaOH or HCO3 to maintain the pH at the setpoint uncomment the function for the right hand side of for the ODE of the biofilter class to run it
 from ChemODE_Fish import chemODE_FISH
 from scipy.integrate import solve_ivp
@@ -11,6 +12,7 @@ from params import params
 from Fish_growth import Weight
 from Biomass_function import Biomass
 from ChemODE_DGS import chemODE_DGS
+
 class Sump:
     def __init__(self, CO2aq, HCO3, CO32, H, OH,NH4, NH3, NO2,V,exchange_rate):
         self.CO2aq = CO2aq
@@ -24,6 +26,7 @@ class Sump:
         self.NO2 = NO2
         self.exchange_rate = exchange_rate
 
+
 class Dosing_pump:
     def __init__(self, OH, dosing_compartment,V):
         self.OH = OH
@@ -35,6 +38,7 @@ class Dosing_pump_HCO3:
         self.HCO3 = HCO3
         self.dosing_compartment = dosing_compartment
         self.V=V
+
 
 class fish_tank:
     Fish_Biomass_Max = 50000
@@ -54,13 +58,14 @@ class fish_tank:
         self.Fish_weight = Fish_weight
         self.Fish_number = Fish_number
         self.Fish_Biomass = Fish_Biomass
+        self.CO2_fish=[]
+        self.CO2_fish_t=[]
 
     def RHS(self, params, t):
         dY1 = chemODE_FISH(self, params,t)
         dY2 = Weight(self, params, t)
         dY = np.append(dY1, dY2)
         return dY
-
 
 
 
@@ -82,11 +87,14 @@ class biofilter:
         self.AOB = AOB
         self.NOB = NOB
         self.B=B
-
+        self.dosing_amount_OH=[] # store the dosing amount of OH
+        self.dosing_amount_HCO3=[] # store the dosing amount of HCO3
+        self.dosing_time=[]
     def RHS(self, params, t):
-        #dY = chemODE_BIO(self, params,t) #uncomment to run and comment the three other lines (simulation case 1)
-        dY = chemODE_BIO_HCO3(self, params, t) #uncomment to run and comment the three other lines (simulation case 2_HCO3 dosing only)
-        #dY = chemODE_BIO_NaOH(self, params, t) #uncomment to run and comment the three other lines (simulation case 2_NaOH dosing only)
+        #dY = chemODE_BIO(self, params,t) #uncomment to run and comment the three other lines (Jafai et al. 2024 simulation settings)
+        #dY = chemODE_BIO_HCO3(self, params, t) #uncomment to run and comment the three other lines (simulation case 1 -HCO3 dosing only)
+        #dY = chemODE_BIO_NaOH(self, params, t) #uncomment to run and comment the three other lines (simulation case 2 -NaOH dosing only)
+        dY= chemODE_BIO_alk_control(self,params, t) #uncomment to run and comment the three other (simulation case 3 -NaOH & HCO3 dosing based on CO2)
         #dY=chemODE_BIO_pH_control(self,params, t) #uncomment to run and comment the three other lines set a threshold for the pH and control it using eiter OH or HCO3
         return dY
 
@@ -109,7 +117,6 @@ class degasser:
         dY1 = chemODE_DGS(self, params,t)
         return dY1
 
-
 def chem(t, S, params, FishTank, B1, DGS):
     # Assigning values from the array S to the  FishTank
     FishTank.CO2aq = S[0]
@@ -123,7 +130,7 @@ def chem(t, S, params, FishTank, B1, DGS):
     FishTank.Fish_weight = S[8]
     FishTank.Fish_Biomass = S[9]
 
-    # Assigning values to B1
+    # Assigning values to B1 (biofilter compartment)
     B1.CO2aq = S[10]
     B1.HCO3 = S[11]
     B1.CO32 = S[12]
@@ -135,7 +142,7 @@ def chem(t, S, params, FishTank, B1, DGS):
     B1.AOB = S[18]
     B1.NOB = S[19]
 
-    # Assigning values to DGS
+    # Assigning values to DGS (degasser compartment)
     DGS.CO2aq = S[20]
     DGS.HCO3 = S[21]
     DGS.CO32 = S[22]
@@ -154,10 +161,9 @@ def chem(t, S, params, FishTank, B1, DGS):
 
     return dY
 
-
+# when choosing the initial conditions, make sure that the initial conditions are consistent with the chemical equilibrium e.g. you cannot have a higher NH3 concentration than NH4 at pH 7
+# When running scenario 1,2,3 change the initial conditions of HCO3 to start with the targeted alkalinity to obtain the same results as in the paper
 # initial condition Fish tank
-
-
 CO2aq_FT = 10/44.1 #10 mg/L / 44.1 g/mol 10/44.1 mmol/L
 HCO3_FT =200/61.01 #(mg/l)==> mmol.l-1
 CO32_FT =10**-6*10**3
@@ -201,9 +207,9 @@ NO3_DGS =40/62
 
 # initial values for the dosing pump
 #pump_OH
-OH_dosing=2000 #mmol/l
+OH_dosing=2000 #mmol/l (concentration of the dosing NaOH solution)
 #pump_HCO3
-HCO3_dosing=2000
+HCO3_dosing=2000 #mmol/l (concentration of the dosing NaHCO3 solution)
 #value for the sump
 CO2aq_Sump =0
 HCO3_Sump =70/61.01
@@ -224,7 +230,7 @@ B1 = biofilter( CO2aq_B1, HCO3_B1, CO32_B1, H_B1, OH_B1, NH4_B1, NH3_B1,
 DGS = degasser( CO2aq_DGS, HCO3_DGS, CO32_DGS, H_DGS, OH_DGS, NH4_DGS, NH3_DGS,
                NO2_DGS, B1, 700,[])
 
-exchange_rate = (B1.V+DGS.V+FishTank.V)*0.25/(60*60*24)
+exchange_rate = (B1.V+DGS.V+FishTank.V)*0.25/(60*60*24) # 25% of the total volume is exchanged every day this can be change to any % of the total volume
 
 Sump_1= Sump( CO2aq_Sump, HCO3_Sump, CO32_Sump, H_Sump, OH_Sump, NH4_Sump, NH3_Sump, NO2_Sump,V_Sump, exchange_rate)
 DGS = degasser( CO2aq_DGS, HCO3_DGS, CO32_DGS, H_DGS, OH_DGS, NH4_DGS, NH3_DGS, NO2_DGS, B1, 700,Sump_1)
@@ -243,7 +249,7 @@ for cycle in range(0,10): #  solve the system for 10 cycles of 14 days, after ea
     tspan = [14 * 24 * 60 * 60 * cycle, 14 * 24 * 60 * 60 * (cycle + 1)]
 
     MAlocal = solve_ivp(fun=chem, t_span=tspan, y0=S0,
-                        args=(params, FishTank, B1, DGS), method="BDF")
+                        args=(params, FishTank, B1, DGS), method="BDF") # if the solver does not converge try using the method "LSODA" with rtol=1e-12;atol=1e-12
     S0 = MAlocal.y[:, -1].tolist() #reintialize S0 with the last values of the previous cycle
 
     if cycle == 0:
@@ -255,35 +261,32 @@ for cycle in range(0,10): #  solve the system for 10 cycles of 14 days, after ea
     n_out = Biomass(FishTank, MAlocal.t[-1])
     FishTank.Fish_number = FishTank.Fish_number - n_out # update the fish number
     S0[9] = FishTank.Fish_number * FishTank.Fish_weight # update the fish biomass
-
-#plot the results
-plt.plot(MA.t,(MA.y[11]*50.04+MA.y[12]*50.04+MA.y[14]*50.04),label='Alkalinity B1')
-plt.plot(MA.t,(MA.y[1]*50.04+MA.y[2]*50.04+MA.y[4]*50.04),label='Alkalinity Tank')
-plt.plot(MA.t,(MA.y[21]*50.04+MA.y[22]*50.04+MA.y[24]*50.04),label='Alkalinity DGS')
-plt.legend()
-plt.show()
-
 # results can be store in a dataframe for further and easier exploration (e.g. TAN,TIC, CO2_removal... )
 df_results = pd.DataFrame(MA.y.T, columns=['CO2aq_FT', 'HCO3_FT', 'CO32_FT', 'H_FT', 'OH_FT', 'NH4_FT', 'NH3_FT',
                                            'NO2_FT', 'Fishweight_FT', 'Fish_Biomass_FT', 'CO2aq_B1', 'HCO3_B1',
                                            'CO32_B1', 'H_B1', 'OH_B1', 'NH4_B1', 'NH3_B1', 'NO2_B1', 'AOB_B1',
                                            'NOB_B1', 'CO2aq_DGS', 'HCO3_DGS', 'CO32_DGS', 'H_DGS', 'OH_DGS',
                                            'NH4_DGS', 'NH3_DGS', 'NO2_DGS'])
-
-# Add the time data as the first column
-
 df_results.insert(0, 'Time', MA.t)
-df_results.plot(x='Time', y=[ 'NH4_FT', 'NH4_DGS'])
+# all the result are in mmol/L, to convert to mg/L use the molecular weight of the species
+
+M_NH4 = 18.04
+M_NH3 = 17.03
+M_N= 14.01
+M_NO2 = 46.01
+M_CO2 = 44.01
+M_HCO3= 61.01
+# alkalinity is calculated as follow Alkalinity = ([HCO3-] + 2*[CO3--] + [OH-]) * 50.04 to have it in mg/L
+# TAN is calculated as follow TAN = ([NH4] + [NH3])* 14.01 to have it in mg/L
+# NH3 is calculated as NH3-N in mg.l-1 the paper to be compared to experimental data : NH3-N=NH3*14.01
+# Time can be divide by 60*60*24 to have it in days
+# all the results in the paper are concentration in the fish tank but the results can be plot for biofilter and degasser as well
+
+# plot results for CO2 in the fish tank
+plt.plot(df_results['Time']/(60*60*24),df_results['CO2aq_FT']*M_CO2,label='CO2aq_FT')
+plt.xlabel('Time (days)')
+plt.ylabel('CO2 (mg/L)')
 plt.legend()
 plt.show()
-df_results.plot(x='Time', y=[ 'NO2_FT', 'NO2_DGS'])
-plt.legend()
-plt.show()
-df_results.plot(x='Time', y=[ 'H_FT', 'H_B1', 'H_DGS'])
-plt.legend()
-plt.show()
-plt.plot(df_results['Time'],-np.log10(df_results['H_B1']*10**-3))
-plt.show()
-#csv_file_path = ''
-#df_results.to_csv(csv_file_path, index=False)
+
 
